6b1e9ff83c4fb386345989c536d3011f0b5cf274
helen amin <helenamin8@gmail.com>
2022-09-29 17:40:16 +0800

    initial_load

---
+++ src/crypto/config.bat
@@ -0,0 +1,8 @@
+set source_db_user=postgres
+set source_db_password=postgres
+set source_db_server_name=localhost
+set source_db_database_name=crypto
+set target_db_user=postgres
+set target_db_password=postgres
+set target_db_server_name=localhost
+set target_db_database_name=crypto_2
---
+++ src/crypto/test/test_extract_load.py
@@ -0,0 +1,31 @@
+
+    # -- assemble -- 
+
+    # create mock source table 
+
+
+    # create extract sql file 
+ 
+    # -- act -- 
+ 
+    # clean up first 
+
+
+    # -- assert -- 
+
+
+    
+
+    # -- assemble -- 
+
+    # create mock source table 
+ 
+    # create extract sql file 
+
+    # -- act -- 
+
+
+    # clean up first 
+ 
+
+    # -- assert -- 
