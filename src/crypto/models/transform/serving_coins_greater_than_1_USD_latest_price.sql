b3061b7412c56bbce66a89b2509cda2aeb76844f
Kingmoh <rashidmincan@gmail.com>
2022-10-15 16:46:09 +1000

    Modify README.md

--- src/crypto/models/transform/serving_coins_greater_than_1_USD_latest_price.sql
+++ src/crypto/models/transform/serving_coins_greater_than_1_USD_latest_price.sql
@@ -2,11 +2,20 @@ drop table if exists {{ target_table }};
 
 create table {{ target_table }} as 
 	with s as (
-	Select id, max("Date") as latest_date from public.coin_price_history
-	group by id
+		Select 
+			id, 
+			max("Date") as latest_date from public.coin_price_history
+		group by id
 	)
-select "Date", "Price", hist."id" as "Coin", "ingestion_date", "id_date", "latest_date" from public.coin_price_history hist
-right join s on hist."Date" = s."latest_date"
-where hist."Price" > 1
-order by hist."Price" desc;
---join, rename, sorting, where
+	select 
+		"Date", 
+		"Price", 
+		hist."id" as "Coin", 
+		"ingestion_date", 
+		"id_date", 
+		"latest_date"
+	from public.coin_price_history hist
+	right join s on hist."Date" = s."latest_date"
+	where hist."Price" > 1
+	order by hist."Price" desc;
+	--join, rename, sorting, where
